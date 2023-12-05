d0ba170ee0460bcc682573df01589a0f9c0b3ec2
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-05 19:25:38 +0300

    Fix: Your commit message doesn't reflect the changes shown in the diff. A relevant commit would be:
    Commit message:
    
    Add odd/even number checker in 02.js
    
    Introduced a new functionality in 02.js script in lesson-01 directory that generates a random number and checks if it's odd or even. This feature was added to illustrate the usage of if-else logic and modulus operator in JavaScript.

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+
+if (number1 % 2 === 0) {
+    console.log(number1, ":Это четное число");
+} else {
+    console.log(number1, ":Это нечетное число");
+}
