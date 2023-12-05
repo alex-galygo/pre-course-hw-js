b32be6ec3f18ecb30f84b6543c429cfe6d7e9292
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-05 19:47:05 +0300

    Add new file and script tag for lesson-01/05.js
    Added a new file (05.js) under Lesson 1 demonstrating usage of switch case with true. The new script also includes a new addition to the index.html, where the script for the created file has been referenced. The script incorporates decision-making code blocks to encourage learning about program flow control in JavaScript.

---
+++ lesson-01/05.js
@@ -0,0 +1,16 @@
+let rule = 'Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.';
+
+switch (true) {
+    case rule.length < 25:
+        console.log('Все таки нет правил без исключения');
+        break;
+    case rule.length > 25:
+        console.log('У меня 100 пудов все получится');
+        break;
+    case rule.length === 25:
+        console.log('50 на 5');
+        break;
+    default:
+        console.log('Нет правил без исключения');
+        break;
+}
