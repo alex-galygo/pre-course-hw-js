1e9c11a5e72d3e44f11098ecb637230fe66350ab
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-13 22:23:27 +0300

    function corrected

---
+++ lesson-02/04.js
@@ -0,0 +1,9 @@
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if (value1 === value2 && value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+
+giveMeRightCondition(201, 100, 200)
