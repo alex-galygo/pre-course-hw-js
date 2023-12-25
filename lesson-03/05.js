0201df3e86304af094fd8af56b208a943421242b
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-25 18:15:48 +0300

    add solution 5 task

---
+++ lesson-03/05.js
@@ -0,0 +1,31 @@
+const users2 = [
+    {
+        id: 1,
+        user: 'John',
+        surName: 'Doe',
+        address: '123 Main St'
+    },
+    {
+        id: 2,
+        user: 'Jane',
+        surName: 'Smith',
+        address: '456 Elm St'
+    },
+    {
+        id: 3,
+        user: 'Tom',
+        surName: 'Johnson',
+        address: '789 Oak St'
+    },
+];
+
+const newObj = {
+    id: 4,
+    user: 'Ivan',
+    surName: 'Susanin',
+    address: 'Unknown'
+}
+
+users2.push(newObj);
+
+console.log(users2);
