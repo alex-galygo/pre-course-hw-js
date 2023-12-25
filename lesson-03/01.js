c3aeb8fcd96ffa2d75cbc804db0a551cc104ad37
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-25 16:56:56 +0300

    solution 1 task

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,9 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((item) => {
+        return item === "черный" || item === "красный" || item === "желтый";
+    }).join("-");
+}
+
+console.log(createColorString());
